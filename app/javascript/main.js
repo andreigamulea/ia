a585b0444486673ef95bd19e2fd74422329b4e3c
andreigamulea <ayushcellromania@gmail.com>
2023-04-12 00:31:38 +0300

    merge 1 Cauta si 2 Reseteaza cautarea (la 2 se goleste campul, nu se face tabelul cu toate row dar merge bine) este no refresh

---
+++ app/javascript/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('click', function(event) {
+    const resetAndSubmitBtn = document.getElementById('reset-and-submit');
+    if (event.target === resetAndSubmitBtn) {
+      console.log("click merge js");
+      const form = document.querySelector('#pas form');
+      const textField = form.querySelector('input[type="text"]');
+      
+      // Verificați dacă valoarea câmpului de căutare este goală
+      if (textField.value !== '') {
+        textField.value = '';
+      }
+  
+      // Trimiteți formularul
+      form.dispatchEvent(new Event('submit', { cancelable: true }));
+    }
+  });
+  
