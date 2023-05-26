44df1cbb0d61681ea8b43773520da641aa135945
andreigamulea <ayushcellromania@gmail.com>
2023-05-26 03:10:57 +0300

    implementare ahoy

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,6 +3,8 @@ class StatisticsController < ApplicationController
         @visits_per_day = Ahoy::Visit.group_by_day(:started_at).count
         #@time_spent_per_visit = Ahoy::Event.where(name: '$view').average(:time)
         @most_visited_pages = Ahoy::Event.where(name: '$view').group(:properties).count
-        
+        @os_stats = Ahoy::Visit.group(:os).count
+        @device_stats = Ahoy::Visit.group(:device_type).count
+
       end
 end
