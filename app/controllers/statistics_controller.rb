81793714995f27da7c28dde1da73746c666049f5
andreigamulea <ayushcellromania@gmail.com>
2023-05-27 18:26:03 +0300

    statistica user

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,13 +1,10 @@
 class StatisticsController < ApplicationController
     def index
         @visits_per_day = Ahoy::Visit.group_by_day(:started_at).distinct.count(:user_id)
-        
-       
-
         @unique_visits_per_day = Ahoy::Event.where(name: '$visit')
-                                      .group(Ahoy::Event.arel_table[:user_id], Ahoy::Event.arel_table[:time].cast('date'))
-                                      .count.count
-                                     
+                                     .group_by_day("time")
+                                     .select("DATE(time) as date, COUNT(DISTINCT user_id) as unique_visitors")
+                                     .map { |e| [e.date, e.unique_visitors] }.to_h
 
         #@time_spent_per_visit = Ahoy::Event.where(name: '$view').average(:time)
         @most_visited_pages = Ahoy::Event.where(name: '$view').group(:properties).count
