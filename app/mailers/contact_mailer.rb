88dd2bf0d5965809c8b4c7203fb9e855f4d21fcc
andreigamulea <ayushcellromania@gmail.com>
2023-06-05 02:37:38 +0300

    restrucurare meniu

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,12 @@
 class ContactMailer < ApplicationMailer
   def contact_email(name, email, message)
     @name = name
-    @email = email
     @message = message
-
-    mail(to: 'ayushcellromania@gmail.com', subject: 'Mesaj de la ayushcell.ro')
+    @email = email
+  
+    mail(to: 'ayushcellromania@gmail.com', from: 'noreply@ayushcell.ro', subject: 'Mesaj de la ayushcell.ro')
   end
+  
 end
 
   
