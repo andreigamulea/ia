370eb8f3cd759aa2606791a0c8a7a6937c6048f8
andreigamulea <ayushcellromania@gmail.com>
2023-06-05 00:44:02 +0300

    restrucurare meniu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,9 +32,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   #config.assets.compile = true
-  config.action_mailer.delivery_method = :smtp
-
-  
+  config.action_mailer.delivery_method = :smtp  
   config.action_mailer.smtp_settings = {
     :address => 'smtp-relay.sendinblue.com',
     :port => 587,
@@ -44,6 +42,7 @@ Rails.application.configure do
     :domain => 'ayushcell.ro',  # schimba acest domeniu cu domeniul tău
     :enable_starttls_auto => true
   }
+  
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
