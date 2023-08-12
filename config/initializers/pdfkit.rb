454bd1c126de5c972cb852b753257edbd8920b32
andreigamulea <ayushcellromania@gmail.com>
2023-08-13 01:00:33 +0300

    am terminat functionalitatea la accesul la tayv2 pot adauga userii si ei vad videourile

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,2 +1,12 @@
 
   ##config/initializers/pdfkit.rb
+PDFKit.configure do |config|
+    if Rails.env.production?
+      config.wkhtmltopdf = `which wkhtmltopdf`.chomp
+    else
+      # Configurarea pentru Windows
+      config.wkhtmltopdf = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    end
+  end
+  
+  
