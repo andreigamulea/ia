9e4c159418ccbef16ad55f2b6e4a5b16f01a2a39
andreigamulea <ayushcellromania@gmail.com>
2023-09-11 14:41:13 +0300

    cod12 acces video

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,1 +1,10 @@
 
+PDFKit.configure do |config|
+  if Rails.env.production?
+    config.wkhtmltopdf = `which wkhtmltopdf`.chomp
+  else
+    # Configurarea pentru Windows
+    config.wkhtmltopdf = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+  end
+end
+
