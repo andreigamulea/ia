dd5fddf0136506fa8dc94a907c01caa4294a1fb1
andreigamulea <ayushcellromania@gmail.com>
2023-10-09 06:24:03 +0300

    mhx

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,7 +1,7 @@
 
 PDFKit.configure do |config|
   if Rails.env.production?
-    config.wkhtmltopdf = `which wkhtmltopdf`.chomp
+    config.wkhtmltopdf = Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
   else
     # Configurarea pentru Windows
     config.wkhtmltopdf = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
