2905bdc842aa1ea53694af9871495b150cce26b2
andreigamulea <ayushcellromania@gmail.com>
2023-10-09 19:04:47 +0300

    merge zip la facturi

--- app/controllers/paginisites_controller.rb
+++ app/controllers/paginisites_controller.rb
@@ -325,7 +325,8 @@ class PaginisitesController < ApplicationController
 
   def export_to_xlsx_plata_an1
    # 1. Extrage produsele cu coduri de la cod14 la cod25
-   produse = Prod.where(cod: (14..25).map { |i| "cod#{i}" })
+   produse = Prod.where(cod: (14..25).map { |i| "cod#{i}" } + ["cod46"])
+
 
    # Construiește hash-ul de mapare id => pret
    mapare_valori = produse.each_with_object({}) do |prod, hash|
