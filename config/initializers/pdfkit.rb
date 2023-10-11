cdabe5a26c0351170907f6f83a87a71cd64a2d58
andreigamulea <ayushcellromania@gmail.com>
2023-10-11 11:46:26 +0300

    xlsx tayt12

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,8 +1,16 @@
 
-
-###  de verificat daca las ce e mai jos daca merg facturile
 PDFKit.configure do |config|
-  unless Rails.env.production?
+  if Rails.env.production?
+    config.wkhtmltopdf = `which wkhtmltopdf`.chomp
+  else
+    # Configurarea pentru Windows
     config.wkhtmltopdf = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
   end
 end
+
+###  de verificat daca las ce e mai jos daca merg facturile
+#PDFKit.configure do |config|
+  #unless Rails.env.production?
+    #config.wkhtmltopdf = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+  #end
+#end
