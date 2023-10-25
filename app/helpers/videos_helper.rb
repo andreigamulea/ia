d449cc8f0435e197b2d1592b3cbb73bffb594b29
andreigamulea <ayushcellromania@gmail.com>
2023-10-25 16:23:10 +0300

    ..

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,8 @@
 module VideosHelper
+    def has_purchased_product?(user, products)
+        return false unless user
+      
+        ComenziProd.where(user_id: user.id, prod_id: products.map(&:id), validat: "Finalizata").exists?
+      end
+      
 end
