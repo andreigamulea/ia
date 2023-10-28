88f957ddcba649f1ef5c677c296bd27388db8d38
andreigamulea <ayushcellromania@gmail.com>
2023-10-28 04:26:04 +0300

    ..

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,5 @@
 Bugsnag.configure do |config|
   config.api_key = "20189077b17f4f6a488ba1465c161975"
-  config.notify_release_stages = ['development', 'production']
+  config.notify_release_stages = ['production']
+  #config.notify_release_stages = ['development', 'production']
 end
