e088744a0e25051d73143e969ff6789fc6bd6c3e
andreigamulea <ayushcellromania@gmail.com>
2023-10-31 20:15:23 +0200

    ..

--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,6 @@ workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 if ENV['USE_PUMA_WORKERS'] == 'true'
     workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 end
-ECHO is off.
 # Use the `preload_app` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
