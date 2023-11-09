8223d3844ecb6a919737386f786731fe185b12d7
andreigamulea <ayushcellromania@gmail.com>
2023-11-09 20:18:38 +0200

    ..

--- app/controllers/crypto_prices_controller.rb
+++ app/controllers/crypto_prices_controller.rb
@@ -1,21 +1,29 @@
 require 'httparty'
 
 class CryptoPricesController < ApplicationController
-    def show_doge_price
-        begin
-          response = HTTParty.get('https://api.binance.com/api/v3/ticker/price', query: { symbol: 'DOGEUSDT' })
-          puts "Răspuns API: #{response.parsed_response.inspect}"
-          if response.success?
-            # Presupunând că răspunsul este JSON și include un key 'price'
-            @doge_price = response.parsed_response['price']
-          else
-            flash[:error] = "Eroare la obținerea prețului."
-            redirect_to root_path and return
-          end
-        rescue HTTParty::Error => e
-          flash[:error] = "Eroare la obținerea prețului: #{e.message}"
-          redirect_to root_path and return
-        end
-      end
-      
+  def show_doge_price
+    begin
+      @doge_prices = get_book_ticker('DOGEUSDT')
+      @btc_prices = get_book_ticker('BTCUSDT')
+      @xmr_prices = get_book_ticker('XMRUSDT')
+      @eth_prices = get_book_ticker('ETHUSDT')
+    rescue HTTParty::Error => e
+      flash[:error] = "Eroare la obținerea prețurilor: #{e.message}"
+      redirect_to root_path and return
+    end
+  end
+
+  private
+
+  def get_book_ticker(symbol)
+    response = HTTParty.get('https://api.binance.com/api/v3/ticker/bookTicker', query: { symbol: symbol })
+    if response.success?
+      {
+        bid_price: response.parsed_response['bidPrice'],
+        ask_price: response.parsed_response['askPrice']
+      }
+    else
+      nil
+    end
+  end
 end
--- config/initializers/binance.rb
+++ config/initializers/binance.rb
@@ -1,4 +1,3 @@
 require 'binance-ruby'
-
 Binance::Api::Configuration.api_key = Rails.application.credentials.dig(:binance, :api_key)
 Binance::Api::Configuration.secret_key = Rails.application.credentials.dig(:binance, :secret_key)
