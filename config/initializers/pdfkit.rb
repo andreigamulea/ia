1472893e3e200601d1856dd9522a1adb335ab03d
andreigamulea <ayushcellromania@gmail.com>
2023-11-23 01:38:43 +0200

    ..

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,18 +1,12 @@
-
-
 PDFKit.configure do |config|
-    if Rails.env.production?
-      config.wkhtmltopdf = `which wkhtmltopdf`.chomp
-    else
-      # Configurarea pentru Windows
-      config.wkhtmltopdf = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-    end
+  if Rails.env.production? && !Gem.win_platform?
+    # Doar pentru sisteme non-Windows în producție
+    config.wkhtmltopdf = `which wkhtmltopdf`.chomp
+  else
+    # Configurarea pentru Windows sau pentru dezvoltare non-producție
+    config.wkhtmltopdf = 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
   end
+end
   
-  ###  de verificat daca las ce e mai jos daca merg facturile
-  #PDFKit.configure do |config|
-    #unless Rails.env.production?
-      #config.wkhtmltopdf = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-    #end
-  #end
+  
   
