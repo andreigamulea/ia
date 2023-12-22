a2c2b4113b92b687e9c784b040eed7df5807ad9a
andreigamulea <ayushcellromania@gmail.com>
2023-12-22 02:48:58 +0200

    ..

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_21_132704) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_21_223831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -192,6 +192,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_132704) do
     t.string "cui_firma"
     t.string "reprezentant_firma"
     t.string "calitate_reprezentant"
+    t.text "semnatura_admin"
   end
 
   create_table "cursuri_history", force: :cascade do |t|
