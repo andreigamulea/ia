677b61a56fd0fbd5a640cc909ee404ae722a9420
andreigamulea <ayushcellromania@gmail.com>
2023-12-22 13:30:21 +0200

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
   get 'videos/:id/myvideo3', to: 'videos#myvideo3', as: 'myvideo3' 
   get 'videos/:id/myvideo4', to: 'videos#myvideo4', as: 'myvideo4' 
   get 'videos/:id/myvideo44', to: 'videos#myvideo44', as: 'myvideo44'
+  get 'videos/:id/myvideo444', to: 'videos#myvideo444', as: 'myvideo444'
   get 'videos/:id/myvideo6', to: 'videos#myvideo6', as: 'myvideo6' 
   get 'videos/:id/myvideo67', to: 'videos#myvideo67', as: 'myvideo67'
   get 'videos/:id/myvideo7', to: 'videos#myvideo7', as: 'myvideo7'
