6348648ee4291728d9b379c547e4bb71aa7e5c09
andreigamulea <ayushcellromania@gmail.com>
2024-01-24 00:59:37 +0200

    ..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -43,10 +43,10 @@ class HomeController < ApplicationController
   def vajikarana
   end  
   def testjivaka
-    @has_access = current_user&.role == 1     
+    @has_access = current_user&.role == 1
     if current_user
       # Selectează toate produsele care corespund criteriilor
-      eligible_prods = Prod.where(curslegatura: 'traditia_ayurvedica', status: 'activ').pluck(:id)
+      eligible_prods = Prod.where(curslegatura: 'jivaka', status: 'activ').pluck(:id)
     
       # Obține ID-urile produselor cumpărate de current_user, care sunt valide și a căror datasfarsit este azi sau în viitor
       purchased_prod_ids = ComenziProd.where(user_id: current_user.id, 
@@ -63,13 +63,13 @@ class HomeController < ApplicationController
       coduri_produse_cumparate = @prods_cumparate.pluck(:cod) # Scoate codurile din produsele cumpărate: cod91..
       #pluck(:cod) este folosit pentru a obține un array de valori din coloana cod a produselor cumpărate.
       if current_user.role==1
-        @videos_correspondente = Video.where(tip: "traditia_ayurvedica").order(:ordine)
+        @videos_correspondente = Video.where(tip: "jivaka").order(:ordine)
 
       else  
       @videos_correspondente = Video.where(cod: coduri_produse_cumparate).order(:ordine)
       end
     else
-      @prods = Prod.where(curslegatura: 'traditia_ayurvedica', status: 'activ').order(:id)
+      @prods = Prod.where(curslegatura: 'jivaka', status: 'activ').order(:id)
       # În cazul în care nu există un current_user, @prods_cumparate va fi gol
       @prods_cumparate = Prod.none
       @videos_correspondente= Video.none
@@ -79,7 +79,7 @@ class HomeController < ApplicationController
     @has_access = current_user&.role == 1     
     if current_user
       # Selectează toate produsele care corespund criteriilor
-      eligible_prods = Prod.where(curslegatura: 'traditia_ayurvedica', status: 'activ').pluck(:id)
+      eligible_prods = Prod.where(curslegatura: 'cognitronica', status: 'activ').pluck(:id)
     
       # Obține ID-urile produselor cumpărate de current_user, care sunt valide și a căror datasfarsit este azi sau în viitor
       purchased_prod_ids = ComenziProd.where(user_id: current_user.id, 
@@ -96,13 +96,13 @@ class HomeController < ApplicationController
       coduri_produse_cumparate = @prods_cumparate.pluck(:cod) # Scoate codurile din produsele cumpărate: cod91..
       #pluck(:cod) este folosit pentru a obține un array de valori din coloana cod a produselor cumpărate.
       if current_user.role==1
-        @videos_correspondente = Video.where(tip: "traditia_ayurvedica").order(:ordine)
+        @videos_correspondente = Video.where(tip: "cognitronica").order(:ordine)
 
       else  
       @videos_correspondente = Video.where(cod: coduri_produse_cumparate).order(:ordine)
       end
     else
-      @prods = Prod.where(curslegatura: 'traditia_ayurvedica', status: 'activ').order(:id)
+      @prods = Prod.where(curslegatura: 'cognitronica', status: 'activ').order(:id)
       # În cazul în care nu există un current_user, @prods_cumparate va fi gol
       @prods_cumparate = Prod.none
       @videos_correspondente= Video.none
