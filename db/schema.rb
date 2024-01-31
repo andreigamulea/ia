ef55e451c86c56ca570f6407b620ccc545de6dba
andreigamulea <ayushcellromania@gmail.com>
2024-01-31 17:49:19 +0200

    ..

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_30_022136) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_31_153605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -216,6 +216,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_30_022136) do
     t.string "relatii_functionale"
     t.integer "contor_start"
     t.integer "valabilitate_luni"
+    t.text "sarcini_voluntar"
   end
 
   create_table "cursuri_history", force: :cascade do |t|
