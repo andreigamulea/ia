475848b7d7b177f038c007cae3389b9e9335da57
andreigamulea <ayushcellromania@gmail.com>
2024-01-31 22:45:43 +0200

    ..

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -84,26 +84,40 @@ end
 
     #@has_access_cursuri = ComenziProd.exists?(user_id: current_user.id, prod_id: 56)
     #verific daca userul poate vedea videourile taberei(a platit?)
-    @has_access_cursuri = if current_user 
+    @has_access_cursuri = if current_user
       prod_id_cod54 = Prod.find_by(cod: 'cod54')&.id
-      current_user.role == 1 || (prod_id_cod54 && ComenziProd.exists?(user_id: current_user.id, prod_id: prod_id_cod54, validat: 'Finalizata'))
+      current_user.role == 1 || (
+        prod_id_cod54 && 
+        ComenziProd.where(user_id: current_user.id, prod_id: prod_id_cod54, validat: 'Finalizata')
+                   .where('datasfarsit >= ?', Date.current)
+                   .exists?
+      )
     end
     
-    @has_access_cursuri2 = if current_user 
-      puts("Avem @has_access_cursuri2=true")
+    puts("@has_access_cursuri este: #{@has_access_cursuri}")
+    
+    @has_access_cursuri2 = if current_user
       prod_id_cod64 = Prod.find_by(cod: 'cod64')&.id
-      current_user.role == 1 || (prod_id_cod64 && ComenziProd.exists?(user_id: current_user.id, prod_id: prod_id_cod64, validat: 'Finalizata'))
+      current_user.role == 1 || (
+        prod_id_cod64 && 
+        ComenziProd.where(user_id: current_user.id, prod_id: prod_id_cod64, validat: 'Finalizata')
+                   .where('datasfarsit >= ?', Date.current)
+                   .exists?
+      )
     end
+    
+    Rails.logger.info("Avem @has_access_cursuri2=true") if @has_access_cursuri2
+    
     
     if !@has_access
-      @prod_tayt12a = Prod.none
-    elsif !@has_access_cursuri && !@has_access_cursuri2
+      @prod_tayt12a = Prod.where(cod: ['cod40', 'cod41','cod42','cod43', 'cod44','cod45'], status: 'activ').order(:luna)
+    elsif !@has_access_cursuri && !@has_access_cursuri2 #a cumparat in 2023 tabara fara nici o parte digitala
       @prod_tayt12a = Prod.where(cod: ['cod54', 'cod64'], status: 'activ').order(:cod)
-    elsif @has_access_cursuri && !@has_access_cursuri2
+    elsif @has_access_cursuri && !@has_access_cursuri2 #a cumparat in 2023 tabara doar cu p1 digitala activa inca
       @prod_tayt12a = Prod.where(cod: 'cod64', status: 'activ').order(:cod)
-    elsif !@has_access_cursuri && @has_access_cursuri2
+    elsif !@has_access_cursuri && @has_access_cursuri2 #a cumparat in 2023 tabara doar cu p2 digitala activa inca
       @prod_tayt12a = Prod.where(cod: 'cod54', status: 'activ').order(:cod)
-    elsif @has_access_cursuri && @has_access_cursuri2
+    elsif @has_access_cursuri && @has_access_cursuri2  #a cumparat in 2023 tabara  cu p1 si p2 digitale activa inca
       @prod_tayt12a = Prod.none
     end
     
