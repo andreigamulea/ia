2845e7b3a7c77b656fb4ca29507cac8bab2a288a
andreigamulea <ayushcellromania@gmail.com>
2024-02-11 00:16:30 +0200

    ..

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -1,7 +1,7 @@
 class PaymentMailer < ApplicationMailer
     def payment_success_email(factura)
       @factura = factura
-      mail(to: "ayushcellromania@gmail.com", subject: 'Factura noua pe ayushcell.ro', from: 'Facturi_ayushcell.ro@ayushcell.ro')
+      mail(to: "ayushcellromania@gmail.com", subject: 'Factura noua pe ayushcell.ro', from: 'Facturi-ayushcell.ro@ayushcell.ro')
     end
   end
   
