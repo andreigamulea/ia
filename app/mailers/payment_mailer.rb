9683542006ea4015a8e1f6e0fecf6e03f28c79bf
andreigamulea <ayushcellromania@gmail.com>
2024-02-11 12:50:53 +0200

    ..

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -1,7 +1,7 @@
 class PaymentMailer < ApplicationMailer
     def payment_success_email(factura)
       @factura = factura
-      if @factura.cui.present? && @factura.nume_companie.present? 
+      if @factura.cui.present? || @factura.nume_companie.present? 
         mail(to: "ayushcellromania@gmail.com", subject: 'Factura noua pe ayushcell.ro', from: 'Factura-pe-firma@ayushcell.ro')
       else
         mail(to: "ayushcellromania@gmail.com", subject: 'Factura noua pe ayushcell.ro', from: 'Factura-pers-fizica@ayushcell.ro')
