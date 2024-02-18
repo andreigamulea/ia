635953714a194a9c625475b2536ecc3261668d8d
andreigamulea <ayushcellromania@gmail.com>
2024-02-18 09:25:14 +0200

    ..

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  Rails.application.config.hosts << "e289-86-120-188-230.ngrok-free.app"
+  Rails.application.config.hosts << "9229-86-120-188-230.ngrok-free.app"
 
   # Enable server timing
   config.server_timing = true
