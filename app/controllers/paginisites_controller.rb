ce1b80b27eb46ee4fb12ce59ed7b5f56428a95ac
andreigamulea <ayushcellromania@gmail.com>
2024-03-01 10:54:01 +0200

    ..

--- app/controllers/paginisites_controller.rb
+++ app/controllers/paginisites_controller.rb
@@ -826,6 +826,8 @@ end
       
       @comenzi_prod = ComenziProd.includes(:user, :prod, comanda: :adresa_comenzi)
                                  .where(prod_id: prod_ids, validat: "Finalizata")
+                                 .order(:created_at)
+      
       # Optimize querying for Detaliifacturare
       user_ids = @comenzi_prod.map(&:user_id).uniq
       detaliifacturare_hash = Detaliifacturare.where(user_id: user_ids).index_by(&:user_id)
