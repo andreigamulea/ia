2e936df0c4fe6dc0381e16683cab45a0602f62ed
andreigamulea <ayushcellromania@gmail.com>
2024-03-01 02:36:11 +0200

    ..

--- app/models/prod.rb
+++ app/models/prod.rb
@@ -3,4 +3,6 @@ class Prod < ApplicationRecord
     has_many :comenzi, through: :comenzi_prods, source: :comanda
     has_many :userprods
     has_many :users, through: :userprods
+
+    has_many :comenzi_prod1s
 end
