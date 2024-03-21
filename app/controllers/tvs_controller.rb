dc8eea91c5f35b945d43a41a23f30cf91e54fa82
andreigamulea <ayushcellromania@gmail.com>
2024-03-21 04:04:23 +0200

    ..

--- app/controllers/tvs_controller.rb
+++ app/controllers/tvs_controller.rb
@@ -4,18 +4,135 @@ class TvsController < ApplicationController
   # GET /tvs or /tvs.json
 
   def canal1
+    now_bucharest = Time.current.in_time_zone('Europe/Bucharest')
+    @now_bucharest = now_bucharest
+   
+
+    @myvideo1 = Tv.where(canal: 1)
+              .where("datainceput <= ? AND datasfarsit >= ?", now_bucharest.to_date, now_bucharest.to_date)
+              .to_a
+              .detect do |tv|
+                # Ajustăm orainceput la data curentă
+                ora_inceput_ajustata = tv.orainceput.change(year: now_bucharest.year, month: now_bucharest.month, day: now_bucharest.day, zone: 'Europe/Bucharest')
+                
+                # Dacă datasfarsit este aceeași zi cu datainceput, ajustăm orasfarsit la aceeași zi
+                if tv.datasfarsit == tv.datainceput
+                  ora_sfarsit_ajustata = tv.orasfarsit.change(year: now_bucharest.year, month: now_bucharest.month, day: now_bucharest.day, zone: 'Europe/Bucharest')
+                else
+                  # Dacă datasfarsit este diferită, ajustăm orasfarsit la datasfarsit
+                  ora_sfarsit_ajustata = tv.orasfarsit.change(year: tv.datasfarsit.year, month: tv.datasfarsit.month, day: tv.datasfarsit.day, zone: 'Europe/Bucharest')
+                end
+
+                # Comparam acum cu orele ajustate
+                result = now_bucharest >= ora_inceput_ajustata && now_bucharest <= ora_sfarsit_ajustata
+                puts "Comparând: Acum - #{now_bucharest}, Început ajustat - #{ora_inceput_ajustata}, Sfârșit ajustat - #{ora_sfarsit_ajustata}. Rezultat: #{result}"
+                
+                result
+              end
+
+      puts "Video selectat: #{@myvideo1 ? @myvideo1.id : 'Niciunul'}"
+      # Setează variabilele în funcție de rezultatul interogării
+      if @myvideo1
+        @myvideo = @myvideo1.link
+        @exista_video = true
+        @denumire = @myvideo1.denumire      
+        @data_sfarsit = @myvideo1.datasfarsit.strftime("%d.%m.%Y") if @myvideo1&.datasfarsit
+
+        @valabilitate_ora_sfarsit = @myvideo1.orasfarsit.strftime("%H:%M") if @myvideo1.orasfarsit      
+        
+      else
+        @myvideo1 = nil
+        @exista_video = false
+      end
 
   end
   def canal2
-    @myvideo1 = Tv.second
-    @myvideo = @myvideo1.link if @myvideo1
+    now_bucharest = Time.current.in_time_zone('Europe/Bucharest')
+    @now_bucharest = now_bucharest
+   
+
+    @myvideo1 = Tv.where(canal: 2)
+              .where("datainceput <= ? AND datasfarsit >= ?", now_bucharest.to_date, now_bucharest.to_date)
+              .to_a
+              .detect do |tv|
+                # Ajustăm orainceput la data curentă
+                ora_inceput_ajustata = tv.orainceput.change(year: now_bucharest.year, month: now_bucharest.month, day: now_bucharest.day, zone: 'Europe/Bucharest')
+                
+                # Dacă datasfarsit este aceeași zi cu datainceput, ajustăm orasfarsit la aceeași zi
+                if tv.datasfarsit == tv.datainceput
+                  ora_sfarsit_ajustata = tv.orasfarsit.change(year: now_bucharest.year, month: now_bucharest.month, day: now_bucharest.day, zone: 'Europe/Bucharest')
+                else
+                  # Dacă datasfarsit este diferită, ajustăm orasfarsit la datasfarsit
+                  ora_sfarsit_ajustata = tv.orasfarsit.change(year: tv.datasfarsit.year, month: tv.datasfarsit.month, day: tv.datasfarsit.day, zone: 'Europe/Bucharest')
+                end
+
+                # Comparam acum cu orele ajustate
+                result = now_bucharest >= ora_inceput_ajustata && now_bucharest <= ora_sfarsit_ajustata
+                puts "Comparând: Acum - #{now_bucharest}, Început ajustat - #{ora_inceput_ajustata}, Sfârșit ajustat - #{ora_sfarsit_ajustata}. Rezultat: #{result}"
+                
+                result
+              end
+
+      puts "Video selectat: #{@myvideo1 ? @myvideo1.id : 'Niciunul'}"
+      # Setează variabilele în funcție de rezultatul interogării
+      if @myvideo1
+        @myvideo = @myvideo1.link
+        @exista_video = true
+        @denumire = @myvideo1.denumire      
+        @data_sfarsit = @myvideo1.datasfarsit.strftime("%d.%m.%Y") if @myvideo1&.datasfarsit
+        @valabilitate_ora_sfarsit = @myvideo1.orasfarsit.strftime("%H:%M") if @myvideo1.orasfarsit     
+        
+      else
+        @myvideo1 = nil
+        @exista_video = false
+      end
+
   end
+  
   def canal3
-    @myvideo1 = Tv.first
-    @myvideo = @myvideo1.link if @myvideo1
+    now_bucharest = Time.current.in_time_zone('Europe/Bucharest')
+    @now_bucharest = now_bucharest
+   
+
+    @myvideo1 = Tv.where(canal: 3)
+              .where("datainceput <= ? AND datasfarsit >= ?", now_bucharest.to_date, now_bucharest.to_date)
+              .to_a
+              .detect do |tv|
+                # Ajustăm orainceput la data curentă
+                ora_inceput_ajustata = tv.orainceput.change(year: now_bucharest.year, month: now_bucharest.month, day: now_bucharest.day, zone: 'Europe/Bucharest')
+                
+                # Dacă datasfarsit este aceeași zi cu datainceput, ajustăm orasfarsit la aceeași zi
+                if tv.datasfarsit == tv.datainceput
+                  ora_sfarsit_ajustata = tv.orasfarsit.change(year: now_bucharest.year, month: now_bucharest.month, day: now_bucharest.day, zone: 'Europe/Bucharest')
+                else
+                  # Dacă datasfarsit este diferită, ajustăm orasfarsit la datasfarsit
+                  ora_sfarsit_ajustata = tv.orasfarsit.change(year: tv.datasfarsit.year, month: tv.datasfarsit.month, day: tv.datasfarsit.day, zone: 'Europe/Bucharest')
+                end
+
+                # Comparam acum cu orele ajustate
+                result = now_bucharest >= ora_inceput_ajustata && now_bucharest <= ora_sfarsit_ajustata
+                puts "Comparând: Acum - #{now_bucharest}, Început ajustat - #{ora_inceput_ajustata}, Sfârșit ajustat - #{ora_sfarsit_ajustata}. Rezultat: #{result}"
+                
+                result
+              end
+
+      puts "Video selectat: #{@myvideo1 ? @myvideo1.id : 'Niciunul'}"
+      # Setează variabilele în funcție de rezultatul interogării
+      if @myvideo1
+        @myvideo = @myvideo1.link
+        @exista_video = true
+        @denumire = @myvideo1.denumire      
+        @data_sfarsit = @myvideo1.datasfarsit.strftime("%d.%m.%Y") if @myvideo1&.datasfarsit
+        @valabilitate_ora_sfarsit = @myvideo1.orasfarsit.strftime("%H:%M") if @myvideo1.orasfarsit
+      else
+        @myvideo1 = nil
+        @exista_video = false
+      end
+
   end  
+
   def index
-    @tvs = Tv.all
+    @tvs = Tv.order(created_at: :desc)
   end
 
   # GET /tvs/1 or /tvs/1.json
