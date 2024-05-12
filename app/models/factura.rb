9b8100fd1befbf67a938f76eb9214e63715170fe
andreigamulea <ayushcellromania@gmail.com>
2024-05-12 16:35:28 +0300

    ..

--- app/models/factura.rb
+++ app/models/factura.rb
@@ -1,4 +1,7 @@
 class Factura < ApplicationRecord
   belongs_to :comanda
   belongs_to :user
+  def self.achitata
+    Factura.where("status IS NULL OR status != ?", "anulata").update_all(status: 'Achitata')
+  end
 end
