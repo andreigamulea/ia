5ee73dbcb168ff3617b2f58ce0e7fdf44461b5b9
andreigamulea <ayushcellromania@gmail.com>
2024-06-03 23:22:59 +0300

    ..

--- app/controllers/facturas_controller.rb
+++ app/controllers/facturas_controller.rb
@@ -124,7 +124,8 @@ class FacturasController < ApplicationController
     #facturas = Factura.where(numar: 1713..1782)#ianuarie
     #facturas = Factura.where(numar: 1780..1922)#februarie
     #facturas = Factura.where(numar: 1918..2037)#martie
-    facturas = Factura.where(numar: 2035..2118)#aprilie
+    #facturas = Factura.where(numar: 2035..2118)#aprilie
+    facturas = Factura.where(numar: 2118..2250)#amai
     # Generați PDF-uri pentru fiecare factură
     pdf_files = facturas.map do |factura|
       @factura = factura
