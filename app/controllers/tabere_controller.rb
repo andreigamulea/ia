8fc9d15aa7c98ed1fd51839074535de8e461823b
andreigamulea <ayushcellromania@gmail.com>
2024-06-26 21:43:29 +0300

    ..

--- app/controllers/tabere_controller.rb
+++ app/controllers/tabere_controller.rb
@@ -93,7 +93,7 @@ class TabereController < ApplicationController
         @myvideo13 = Video.none
       end
     else
-      # Utilizator neautentificat
+      ## Utilizator neautentificat
       puts("User nelogat")
       @prods = Prod.where(curslegatura: 'nutritie4', status: 'activ').where(cod: ['cod85', 'cod86']).order(:id)
       @has_access = false
