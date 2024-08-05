36a64f18a871d8401f5552aea92543f8ace00c8c
andreigamulea <ayushcellromania@gmail.com>
2024-08-05 17:23:55 +0300

    ..

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,10 +8,10 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.config = {
-  #exe_path: '/usr/local/bin/wkhtmltopdf'
-  exe_path: 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-
+#WickedPdf.config = {
+  WickedPdf.configure do |config|
+    config.exe_path = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+  end
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
@@ -30,4 +30,5 @@ WickedPdf.config = {
   # 'xvfb-run' command, in order to simulate an X server.
   #
   # use_xvfb: true,
-}
+#}
+
