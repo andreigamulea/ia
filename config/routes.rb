2596668ede68b507b1970555f36bb9a977a1a89c
andreigamulea <ayushcellromania@gmail.com>
2024-08-09 11:17:26 +0300

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -338,6 +338,7 @@ post '/stripe-webhooks', to: 'stripe_webhooks#create'
   get 'rasayana', to: 'home#rasayana' 
   get 'newsletter', to: 'home#newsletter'
   
+  get 'copyright', to: 'home#copyright'
   get 'documente', to: 'home#documente'
   get 'user77', to: 'home#user77'
   get 'evaluare_tipologie_ayurvedica', to: 'home#evaluare_tipologie_ayurvedica'
