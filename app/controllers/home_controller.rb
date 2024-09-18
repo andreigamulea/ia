ae6cf93b5eb1d40c51fca374abacb2c55875ee42
andreigamulea <ayushcellromania@gmail.com>
2024-09-18 16:58:33 +0300

    ..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,30 +13,27 @@ class HomeController < ApplicationController
   def politica_gdpr
   end 
   def test
-    if request.referer && request.referer.include?("www.ayushcell.ro")
-      s3_bucket = 'ayushcell-videos'
-      s3_key = 'test/Forest_Waterfall_Nature_Sounds_1_Hour_Relaxing_Birds_Chirping_River.mp4'
-  
-      # Creează un presigner pentru a genera URL-ul semnat temporar
-      presigner = Aws::S3::Presigner.new(client: S3_CLIENT)
-  
-      # Generează URL-ul semnat temporar pentru video
-      video_url = presigner.presigned_url(:get_object, 
-                                           bucket: s3_bucket, 
-                                           key: s3_key, 
-                                           expires_in: 28800, 
-                                           response_content_disposition: 'inline')
-  
-      # Obține video-ul din Wasabi
-      video_object = S3_CLIENT.get_object(bucket: s3_bucket, key: s3_key)
-  
-      # Trimite conținutul video-ului înapoi la utilizator
-      send_data video_object.body.read, filename: s3_key.split('/').last, type: 'video/mp4', disposition: 'inline'
-    else
-      render plain: "Acces interzis", status: :forbidden
-    end
+    s3_bucket = 'ayushcell-videos'
+    s3_key = 'test/Forest_Waterfall_Nature_Sounds_1_Hour_Relaxing_Birds_Chirping_River.mp4'
+    subtitle_key = 'test/Forest_Waterfall_Nature_Sounds_1_Hour_Relaxing_Birds_Chirping_River.vtt'
+
+    # Creează un presigner pentru a genera URL-ul semnat temporar
+    presigner = Aws::S3::Presigner.new(client: S3_CLIENT)
+
+    # Generează URL-ul semnat temporar pentru video
+    @video_url = presigner.presigned_url(:get_object, 
+                                          bucket: s3_bucket, 
+                                          key: s3_key, 
+                                          expires_in: 28800, 
+                                          response_content_disposition: 'inline')
+
+    # Generează URL-ul semnat temporar pentru subtitrări
+    @subtitle_url = presigner.presigned_url(:get_object, 
+                                             bucket: s3_bucket, 
+                                             key: subtitle_key, 
+                                             expires_in: 28800, 
+                                             response_content_disposition: 'inline')
   end
-  
   def panouadmin
     @newsauupdate = 0 #acest cod este pt preluarea datelor de facturare pt an3 atat. 
     email_exists_in_listacanal3 = Listacanal3.exists?(email: current_user&.email)
