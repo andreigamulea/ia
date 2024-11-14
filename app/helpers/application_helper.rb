0262e15f6196116c74f9d8411ee6bc738f9c7cfb
andreigamulea <ayushcellromania@gmail.com>
2024-11-15 01:22:42 +0200

    ..

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  SECRET_KEY = "secretkey1"
     def set_selected_values_to_session(selected_values)
       session[:selected_values] = selected_values.map do |value|
         { id: value[:id], value: value[:value] }
