0de9182954f43a305e786509f2e8611f82832d43
andreigamulea <ayushcellromania@gmail.com>
2025-01-29 16:39:37 +0200

    ..

---
+++ app/services/deepl_service.rb
@@ -0,0 +1,31 @@
+require 'faraday'
+require 'json'
+
+class DeeplService
+  BASE_URL = "https://api-free.deepl.com/v2/translate"
+
+  def initialize
+    @api_key = Rails.application.credentials.dig(:deepl, :api_key)
+  end
+
+  def translate(text, target_lang = "EN", source_lang = "RO")
+    return unless text.present?
+
+    response = Faraday.post(BASE_URL) do |req|
+      req.headers['Authorization'] = "DeepL-Auth-Key #{@api_key}"
+      req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+      req.body = URI.encode_www_form(
+        text: text,
+        target_lang: target_lang,
+        source_lang: source_lang
+      )
+    end
+
+    if response.success?
+      JSON.parse(response.body)["translations"].first["text"]
+    else
+      Rails.logger.error "DeepL API error: #{response.body}"
+      nil
+    end
+  end
+end
