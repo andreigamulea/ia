4780045642090eff680f4c9d54f40584c7b4ad6d
andreigamulea <ayushcellromania@gmail.com>
2025-02-14 18:05:34 +0200

    ..

--- app/controllers/tayt12_controller.rb
+++ app/controllers/tayt12_controller.rb
@@ -11,7 +11,7 @@ class Tayt12Controller < ApplicationController
     end
     
     # Logica acțiunii pentru utilizatorii autentificați.
-    if current_user && (current_user.email== 'sofiacassiano@yahoo.com')
+    if current_user && (current_user.email== 'braferdes@gmail.com')
       @prod_tayt12 = Prod.where(cod: "cod48")
     else
       @prod_tayt12 = Prod.where(cod: "cod63")
