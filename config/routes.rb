28c46acd08e44ad0d7a19c4cc7905b8886673ca9
andreigamulea <ayushcellromania@gmail.com>
2025-03-05 12:49:07 +0200

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
 
   get 'cursuri_ayurveda/ayurveda_padartha', to: 'cursuri_ayurveda#ayurveda_padartha', as: 'ayurveda_padartha'
+  get 'cursuri_ayurveda/export_to_xlsx_plata_ayurveda_padartha', to: 'cursuri_ayurveda#export_to_xlsx_plata_ayurveda_padartha', as: 'export_xlsx_plata_ayurveda_padartha'
 
 
   get 'rasayana/modul1', to: 'rasayana#modul1', as: 'rasayana_modul1'
